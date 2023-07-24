5b2c62c2057cb0f18cadf3fd4265c6c9ed14007d
Ariel Sardinha <ariel.sardinha.dev@gmail.com>
2023-07-24 18:03:16 -0300

    analisador de numeros

---
+++ script.js
@@ -0,0 +1,59 @@
+let num = document.querySelector('#num')
+let lista = document.querySelector('#lista')
+let res = document.querySelector('#res')
+let valores = []
+
+function isnumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inlista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isnumero(num.value) && !inlista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `valor ${num.value} adicionado`
+        lista.appendChild(item)
+    } else {
+        window.alert('Valor invalido ou ja encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('adicione um valor antes de finalizar')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo , temos ${tot} numeros cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>o menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>ASomando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A media ds valores digitados eh ${media}</p>`
+    }
+}
